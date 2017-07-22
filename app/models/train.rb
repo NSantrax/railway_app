388208fd5e66bc5dcd54b6b2c1e42cf23d347e1c
zapad7715 <zapad7715@ya.ru>
2017-07-22 12:24:03 +0300

    Rubocop

--- app/models/train.rb
+++ app/models/train.rb
@@ -1,5 +1,5 @@
 class Train < ApplicationRecord
   belongs_to :route
-  belongs_to :current_station, class_name: 'RailwayStation'
+  belongs_to :current_station, class_name: "RailwayStation"
   has_many :tickets
 end
