07b59616439799a7a1b49184677591055c6395e7
zapad7715 <zapad7715@ya.ru>
2017-07-23 09:52:43 +0300

    The ticket is created without reference to the user

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,5 +2,5 @@ class Ticket < ApplicationRecord
   belongs_to :end_station, class_name: "RailwayStation", foreign_key: :end_station_id
   belongs_to :start_station, class_name: "RailwayStation", foreign_key: :start_station_id
   belongs_to :train
-  belongs_to :user
+  belongs_to :user, optional: true
 end
