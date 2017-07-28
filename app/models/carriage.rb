261fc18eebabc0eaacc07bad68b6192402e35a60
zapad7715 <zapad7715@ya.ru>
2017-07-29 00:36:50 +0300

    Refactoring model Carriage. Add scopes

--- app/models/carriage.rb
+++ app/models/carriage.rb
@@ -5,4 +5,7 @@ class Carriage < ApplicationRecord
   validates :type_class, inclusion: { in: TYPE_CLASS, message: "%{value} is not a valid type of carriage" }
   validates :bottom_seats, numericality: { greater_than_or_equal_to: 0}
   validates :top_seats, numericality: { greater_than_or_equal_to: 0}
+
+  scope :coupe, -> { where(type_class: 'coupe') }
+  scope :platscart, -> { where(type_class: 'platscart') }
 end
