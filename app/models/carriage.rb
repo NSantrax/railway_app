da57257abe05bcc9c245e1e7a5010c3649c09ee3
zapad7715 <zapad7715@ya.ru>
2017-08-11 15:20:16 +0300

    When adding a carriage to the train, automatically assign the carriage the serial number

--- app/models/carriage.rb
+++ app/models/carriage.rb
@@ -1,7 +1,10 @@
 class Carriage < ApplicationRecord
   TYPE_CLASS = %w[ CoupeCarriage EconomyCarriage SvCarriage ChairCarriage ]
   SEATS = %w[ bottom_seats top_seats side_top_seats side_bottom_seats chair_seats ]
+
   belongs_to :train
+
+  before_validation :set_number
 
   validates :type, inclusion: { in: TYPE_CLASS, message: "%{value} is not a valid type of carriage" }
   SEATS.each do |seat|
@@ -15,5 +18,11 @@ class Carriage < ApplicationRecord
 
   def self.total_seats(type, seats)
     send(type).sum(seats)
+  end
+
+  private
+
+  def set_number
+    self.number ||= train.carriages.maximum('number').to_i + 1
   end
 end
