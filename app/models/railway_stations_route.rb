d97a3ff0871a15f07b796710f8e6015dfc4e7936
zapad7715 <zapad7715@ya.ru>
2017-08-27 00:57:47 +0300

    Complete sort stations in the route.

--- app/models/railway_stations_route.rb
+++ app/models/railway_stations_route.rb
@@ -1,4 +1,7 @@
 class RailwayStationsRoute < ApplicationRecord
   belongs_to :railway_station
   belongs_to :route
+
+  validates :route_id, :railway_station_id, presence: true
+  validates :position, numericality: { greater_than_or_equal_to: 0, only_integer: true }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170826144230) do
+ActiveRecord::Schema.define(version: 20170826204525) do
 
   create_table "carriages", force: :cascade do |t|
     t.integer "number"
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 20170826144230) do
   create_table "railway_stations_routes", force: :cascade do |t|
     t.integer "railway_station_id"
     t.integer "route_id"
-    t.integer "station_position"
+    t.integer "position"
   end
 
   create_table "routes", force: :cascade do |t|
