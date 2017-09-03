3af83f8953aaf4c2ce2c461b8719f7dc5f8ea43d
zapad7715 <zapad7715@ya.ru>
2017-09-03 22:06:45 +0300

    When buying a ticket, the ticket is bound to the logged-in user

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,14 +1,15 @@
 class TicketsController < ApplicationController
   before_action :set_ticket, only: [:show]
+  before_action :authenticate_user!
 
   def new
-    @ticket = Ticket.new
+    @ticket = current_user.tickets.new
   end
 
   def create
-    @ticket = Ticket.new(ticket_params)
+    @ticket = current_user.tickets.new(ticket_params)
     if @ticket.save
-      redirect_to @ticket
+      redirect_to @ticket, notice: "Ticket successfully created."
     else
       render :new
     end
@@ -25,7 +26,7 @@ class TicketsController < ApplicationController
 
     def ticket_params
       params.require(:ticket).permit(
-        :id, :train_id, :user_id,
+        :id, :train_id,
         :last_name, :first_name, :middle_name,
         :passport_number, :start_station_id, :end_station_id
       )
