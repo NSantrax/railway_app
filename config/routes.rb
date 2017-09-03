01552fae371a80fce3b1ca5c504d039dbf48bae7
zapad7715 <zapad7715@ya.ru>
2017-09-03 21:42:05 +0300

    The application is divided into user and administrative parts

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,27 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root "searches#show"
 
-  resources :railway_stations do
-    patch :update_position,  on: :member
-    patch :update_event,     on: :member
+  namespace :admin do
+
+    resources :railway_stations do
+      patch :update_position,  on: :member
+      patch :update_event,     on: :member
+    end
+
+    resources :routes
+
+    resources :trains do
+      resources :carriages, shallow: true
+    end
+
+    resources :chair_carriages,   controller: "carriages", type: "ChairCarriage"
+    resources :coupe_carriages,   controller: "carriages", type: "CoupeCarriage"
+    resources :economy_carriages, controller: "carriages", type: "EconomyCarriage"
+    resources :sv_carriages,      controller: "carriages", type: "SvCarriage"
+
   end
-
-  resources :routes
-
-  resources :trains do
-    resources :carriages, shallow: true
-  end
-
-  resources :chair_carriages,   controller: "carriages", type: "ChairCarriage"
-  resources :coupe_carriages,   controller: "carriages", type: "CoupeCarriage"
-  resources :economy_carriages, controller: "carriages", type: "EconomyCarriage"
-  resources :sv_carriages,      controller: "carriages", type: "SvCarriage"
-
-  resource :search, only: [:show, :create]
-
+  resource  :search,  only: [:show, :create]
   resources :tickets, only: [:new, :create, :show]
 end
