a8f30e011ecf84b8f76d3da6abee2bb3a77c3b36
zapad7715 <zapad7715@ya.ru>
2017-10-05 21:24:07 +0300

    JS for   .field

---
+++ app/assets/javascripts/carriages.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $('#carriage_type').change(function() {
+    var carriage_type;
+
+    carriage_type = '.' + $(this).val();
+    $('.seats').hide();
+    $(carriage_type).show();
+    $('#submit_carriage_form').show();
+  });
+});
--- app/models/carriage.rb
+++ app/models/carriage.rb
@@ -1,12 +1,12 @@
 class Carriage < ApplicationRecord
-  TYPE_CLASS = %w[ CoupeCarriage EconomyCarriage SvCarriage ChairCarriage ]
+  CARRIAGE_TYPES = %w[ CoupeCarriage EconomyCarriage SvCarriage ChairCarriage ]
   SEATS = %w[ bottom_seats top_seats side_top_seats side_bottom_seats chair_seats ]
 
   belongs_to :train
 
   before_validation :set_number
 
-  validates :type, inclusion: { in: TYPE_CLASS, message: "%{value} is not a valid type of carriage" }
+  validates :type, inclusion: { in: CARRIAGE_TYPES, message: "%{value} is not a valid type of carriage" }
   validates :number, uniqueness: { scope: :train_id }
 
   SEATS.each do |seat|
@@ -15,7 +15,7 @@ class Carriage < ApplicationRecord
 
   scope :car_sort, ->(train) { train.car_sort ? order("number ASC") : order("number DESC") }
 
-  TYPE_CLASS.each do |type|
+  CARRIAGE_TYPES.each do |type|
     scope type.downcase.sub("carriage", "").to_sym, -> { where(type: type) }
   end
 
